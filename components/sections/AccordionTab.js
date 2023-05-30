20a0c4f7adc3da8620daf5083015b74600ebb48d
Nitrammets <martinmetskula@gmail.com>
2023-05-30 15:41:01 +0300

    Image change

--- components/sections/AccordionTab.js
+++ components/sections/AccordionTab.js
@@ -12,9 +12,9 @@ function AccordionTab({ img, title }) {
         }}
       >
         {/* Hover Gradient */}
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 group-hover:opacity-90 pointer-events-none"></div>
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 group-hover:opacity-90 pointer-events-none transition-all"></div>
         <div className="w-full flex justify-center h-full items-center">
-          <h2 className="uppercase transition-all text-white text-4xl font-sans  opacity-0 group-hover:opacity-100 z-10">
+          <h2 className="uppercase transition-all text-white text-5xl font-roboto font-semibold  opacity-0 group-hover:opacity-100 z-10">
             {title}
           </h2>
         </div>
