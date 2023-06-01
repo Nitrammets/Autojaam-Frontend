967ac71975cececa3badbe68b6be1c39a6b6d6fc
Nitrammets <martinmetskula@gmail.com>
2023-06-01 18:48:22 +0300

    Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import Header from "@/components/sections/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
